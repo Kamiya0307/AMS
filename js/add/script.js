ae28bd7964cac48bae2b453c7086fb2ea42b8fcc
kosaka <>
2021-12-09 18:18:49 -0800

    jqueryをjsに変更

--- js/add/script.js
+++ js/add/script.js
@@ -1,32 +1,32 @@
-function previewImage1(obj)
-{
-	var fileReader = new FileReader();
-	fileReader.onload = (function() {
-		document.getElementById('img1').src = fileReader.result;
-	});
-	fileReader.readAsDataURL(obj.files[0]);
+const fileInput = document.getElementById("img_upload_button");
+fileInput.addEventListener("change", handleFiles, false);
+
+function handleFiles() {
+  const files = fileInput.files;
+	if (4 < files.length){
+		alert('画像の枚数が４枚より多いため最初の４枚が選択されます。');
+	}
+	const imageLength = 4 < files.length ? 4 : files.length;
+  for (let i = 0; i < imageLength; i++) {
+    console.log(files[i]);
+		previewFile(files[i],i);
+  }
 }
-function previewImage2(obj)
-{
-	var fileReader = new FileReader();
-	fileReader.onload = (function() {
-		document.getElementById('img2').src = fileReader.result;
-	});
-	fileReader.readAsDataURL(obj.files[0]);
-}
-function previewImage3(obj)
-{
-	var fileReader = new FileReader();
-	fileReader.onload = (function() {
-		document.getElementById('img3').src = fileReader.result;
-	});
-	fileReader.readAsDataURL(obj.files[0]);
-}
-function previewImage4(obj)
-{
-	var fileReader = new FileReader();
-	fileReader.onload = (function() {
-		document.getElementById('img4').src = fileReader.result;
-	});
-	fileReader.readAsDataURL(obj.files[0]);
+
+let imgBox = [];
+imgBox.push(document.getElementById("img1"));
+imgBox.push(document.getElementById("img2"));
+imgBox.push(document.getElementById("img3"));
+imgBox.push(document.getElementById("img4"));
+
+function previewFile(file,index) {
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    const imageUrl = e.target.result;
+		imgBox[index].src = imageUrl;
+  }
+
+  // いざファイルをURLとして読み込む
+  reader.readAsDataURL(file);
 }
